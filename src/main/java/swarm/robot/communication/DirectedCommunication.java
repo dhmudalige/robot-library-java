dc23585b72d0603d33a0ffb2b8dbbe4be490125e
Dlshani Karunarathna <dinelkadilshani95@gmail.com>
2021-02-19 13:07:41 +0530

    directional communication

--- src/main/java/swarm/robot/communication/DirectedCommunication.java
+++ src/main/java/swarm/robot/communication/DirectedCommunication.java
@@ -6,6 +6,7 @@ import swarm.robot.Robot;
 
 import java.util.HashMap;
 
+
 public class DirectedCommunication extends Communication {
 
     enum mqttTopic {COMMUNICATION_IN};
@@ -13,16 +14,28 @@ public class DirectedCommunication extends Communication {
 
     public DirectedCommunication(int robotId, RobotMqttClient m){
         super(robotId, m);
-
+        subscribe("comm/in/dir/" + robotId);
     }
 
     @Override
     void subscribe(String topic) {
-
+        topicsSub.put(SimpleCommunication.mqttTopic.COMMUNICATION_IN, topic);      // Put to the queue
+        robotMqttClient.subscribe(topic);   // Subscribe through MqttHandler
     }
 
     @Override
     public void handleSubscription(Robot r, MqttMsg m) {
+        // comm/in/
+        String topic = m.topic;
+        String msg = m.message;
 
+        if (topic.equals(topicsSub.get(SimpleCommunication.mqttTopic.COMMUNICATION_IN))) {
+            // comm/in/{id}
+            // System.out.println("Received: " + topic + "> " + msg);
+            r.communicationInterrupt(msg);
+
+        } else {
+            System.out.println("Received (unknown): " + topic + "> " + msg);
+        }
     }
 }
